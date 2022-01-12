fe8e5011a2a26f47285537a61b32fea483de51f8
Svetlana <kovalenkosveta@ukr.net>
2022-01-12 13:48:20 +0200

    all

--- AUTOTESTS/specs/example.e2e.js
+++ AUTOTESTS/specs/example.e2e.js
@@ -1,12 +1,12 @@
-const desctopePage = require('../../pages/desktopPage.js');
+const desktopPage = require('../../pages/desktopPage.js');
 require("expect-webdriverio").setOptions({ wait: 2000, interval: 500 });
 require('../../node_modules/webdriverio')
 
 // describe('My Login application', () => {
 //     it('should open desctop page', async () => {
-//         await desctopePage.open('desctops');
+//         await desctopPage.open('desctops');
 
-//         await desctopePage.firstElement.clik();
+//         await desctopPage.firstElement.clik();
 //         let text = await desctopePage.price.getText();
 //         await expect(text).toBe('800,00');
 //         });
@@ -15,6 +15,6 @@ require('../../node_modules/webdriverio')
 
 describe('open main page and select desctop computers', () => {
     it('should open main page', async () => {
-        await descropPage.open();
+        await desktopPage.open();
     })
 })
