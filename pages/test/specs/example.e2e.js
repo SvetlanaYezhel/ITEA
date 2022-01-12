c8300c401eb99a1c208d77a1cdb20599deb94d99
Svetlana <kovalenkosveta@ukr.net>
2022-01-12 12:58:39 +0200

    all

---
+++ pages/test/specs/example.e2e.js
@@ -0,0 +1,15 @@
+const desctopePage = require('../../pageobjects/desktopPage.js');
+require("expect-webdriverio").setOptions({ wait: 2000, interval: 500 });
+require('../../node_modules/webdriverio')
+
+// describe('My Login application', () => {
+//     it('should open desctop page', async () => {
+//         await desctopePage.open('desctops');
+
+//         await desctopePage.firstElement.clik();
+//         let text = await desctopePage.price.getText();
+//         await expect(text).toBe('800,00');
+//         });
+// });
+
+
